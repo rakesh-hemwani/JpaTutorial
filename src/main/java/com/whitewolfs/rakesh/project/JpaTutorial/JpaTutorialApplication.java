a1b9b4233a2950b77b6fef71d25194cc6d87167f
Rakesh Hemwani <rakeshhemwani654@gmail.com>
2025-01-02 23:10:18 +0530

    JPA Basics

---
+++ src/main/java/com/whitewolfs/rakesh/project/JpaTutorial/JpaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.whitewolfs.rakesh.project.JpaTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whitewolfs/rakesh/project/JpaTutorial/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitewolfs.rakesh.project.JpaTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
